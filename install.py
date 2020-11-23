31e3bb5f9d4135fcb9818dd79518084b92c174b1
golok_ku <49714008+GOLOKKU@users.noreply.github.com>
2020-11-23 22:16:31 +0700

    Rewrite 0.3
    This code are untested soon i will test it

--- install.py
+++ install.py
@@ -1,80 +1,113 @@
 import os
 import platform
 import sys
-import subprocess as su
+import subprocess as sub
+import requests
+from zipfile import ZipFile as zip
+import tarfile as tar
 
-def findString(filename, string):
-    for i in open(filename, "r"):
-        if string in i:findString.r=i
+#PR For zip and tar alternative are welcome
+#or maybe i will implement it soon
+def unzip(loc, output):
+    if loc.endswith("zip"):
+        zap=zip(loc, 'r')
+        zap.extractall(output)
+        zap.close()
+    elif loc.endswith("tar.gz"):
+        tir = tar.open(loc, "r:gz")
+        tir.extractall(output)
+        tir.close()
+    elif loc.endswith("tar"):
+        tir = tar.open(loc, "r:")
+        tir.extractall(output)
+        tir.close()
 
-#Make execution command shell faster
-def exe(command):
-    os.system(command)
+def download(url, save, msg, long):
+    with open(save, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total = response.headers.get('content-length')
 
-#main
-def main():
-    print("Checking graalvm github please wait : ")
-    exe("curl https://api.github.com/repos/graalvm/graalvm-ce-builds/releases/latest -o get.json")
-    main.x = input("Input java version you want to install (8/11): ")
-    main.systemos=""
-    k = platform.system()
-    print("checking OS please wait")
-    if "com.termux" in sys.executable:
-        print("Termux detected")
-        api()
-        Termux()
-    elif "Linux" in k:
-        main.systemos="linux"
-        a = su.getoutput("cat /etc/*-release")
-        #still looking for alternative
-        a=(str(a).split("\n")[0]).split("DISTRIB_ID=")[1]
-        if "Ubuntu" or "Debian" in a:
-            print("Debian-Ubuntu Detected")
-            api()
-            Debian()
+        if total is None:
+            f.write(response.content)
         else:
-            print("Unable to Start Only support Debian-Ubuntu Linux for this time")
-    elif "Windows" in k:
-        main.systemos="linux"
+            downloaded = 0
+            total = int(total)
+            for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
+                downloaded += len(data)
+                f.write(data)
+                done = int(long*downloaded/total)
+                sys.stdout.write('\r{} [{}{}]'.format(msg,'█' * done, '.' * (long-done)))
+                sys.stdout.flush()
+    sys.stdout.write('\n')
+
+def search(file, string):
+    for i in open(file, "r"):
+        if string in i:
+            search.r=i
+
+def cmd(line):
+    os.system(line)
+
+def mkdir(loc):
+    os.mkdir(loc)
+
+def mv(loc0, loc1):
+    os.rename(loc0, loc1)
+
+def system():
+    pl = platform.system()
+    if "Linux" in pl:
+        system.os="linux"
+        if "com.termux" in sys.executable:
+            system.path = "/data/data/com.termux/files/usr/bin/java/"
+            print("termux detected")
+        else:
+            system.path = "/usr/bin/java/"
+    elif "Windows" in pl:
+        system.path = "C:\java"
+        system.os="windows"
         print("Windows Detected")
-        api()
-        Windows()
     else:
-        print("Unable to Start Only support \nDebian-Ubuntu,Windows,Termux for this time")
+        print("Unable to Start Only support :"
+        "Linux,Windows,Termux for this time "
+        "Report to github if you sure this is are bug")
 
 def api():
-    findString("get.json", f'"name": "graalvm-ce-java{main.x}-{main.systemos}-amd64-')
-    api.v=findString.r[45:-10]
-    api.l=f"graalvm-ce-java{main.x}-{api.v}/"
-    findString("get.json", f"graalvm-ce-java{main.x}-{main.systemos}-amd64-")
-    api.a=findString.r[31:-9]
-    api.z =(f"Graalvm{main.x}.tar.gz")
+    #PR without downloading github api data are welcome
+    #or maybe i will implement it soon
+    download("https://api.github.com/repos/graalvm/graalvm-ce-builds/releases/latest", "Api.json", "Getting Github Data info ", 3)
 
-#Installing on System
-def Debian():
-    exe("sudo apt update ; sudo apt install unzip tar")
-    exe(f"sudo wget {api.a} -o {api.z}")
-    exe(f"tar -xzf {api.z}")
-    exe(f"sudo mkdir /usr/lib/jvm ; sudo mv {api.l} /usr/lib/jvm")
-    exe(f"echo 'export PATH=/usr/lib/jvm/{api.l}/bin:$PATH' >> ~/.bashrc")
-    exe(f"echo 'export JAVA_HOME=/usr/lib/jvm/{api.l}' >> ~/.bashrc")
+    search("get.json", f'"name": "graalvm-ce-java{main.ver}-{system.os}-amd64-')
+    api.name=search.r[15:-10]
+    api.ver=search.r[47:-14]
+    api.file=(f"graalvm-ce-java11-{api.ver}")
+    api.link=(f"https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-{api.ver}/{api.name}")
 
-def Termux():
-    path = "/data/data/com.termux/files/usr/bin/java/"
-    exe("pkg update ; pkg install unzip tar")
-    exe(f"wget {api.a} -o {api.z}")
-    exe(f"tar -xzf {api.z}")
-    exe(f"mkdir {path} ; mv {api.l} {path}")
-    exe(f"echo 'export PATH={path}{api.l}/bin:$PATH' >> ~/.bashrc")
-    exe(f"echo 'export JAVA_HOME={path}{api.l}' >> ~/.bashrc")
+    print(api.ver)
+    print(api.name)
+    print(api.file)
+    print(api.link)
 
-def Windows():
-    ps = "powershell"
-    exe(f"{ps} wget {api.a} -o {api.z}")
-    exe(f"{ps} tar -xzf {api.z}")
-    exe("mkdir C:\Java")
-    exe(f"{ps} mv {api.l}")
-    exe('setx /M PATH "C:\java\bin;%PATH%"')
-    exe('setx /M JAVA_HOME "C:\java"')
+def start():
+    a = system.os
+    download(api.link, api.name, f"Downloading {api.name}", 50)
+    print("download done")
+    unzip(api.name, (os.getcwd()))
+    mkdir(system.path)
+    mv((os.getcwd()), system.path)
+    if "linux" in a:
+        cmd(f"echo 'export PATH={system.path}{api.l}/bin:$PATH' >> ~/.bashrc")
+        cmd(f"echo 'export JAVA_HOME={system.path}{api.l}' >> ~/.bashrc")
+    elif "windows" in a:
+        cmd(f'setx /M PATH "{system.path}\bin;%PATH%"')
+        cmd(f'setx /M JAVA_HOME "{system.path}"')
+
+def main():
+    print("checking graalvm github please wait")
+    main.ver=input("Java version to install 8/11 : ")
+    system()
+    api()
+    start()
+    print("Thank you for using our Graalvm install script")
 
 main()
