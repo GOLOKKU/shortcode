0bc37a3656a411ec3294f87207a9925bbf52581f
golok_ku <49714008+GOLOKKU@users.noreply.github.com>
2021-02-18 17:30:42 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-wget https://raw.githubusercontent.com/GOLOKKU/Graalvm-Java-Install/main/install.py
-a=$(python3 --version)
-if [ -n "$a" ]; then
-  python3 install.py
-else
-  apt install python3
-  python3 install.py
-fi
-bash
